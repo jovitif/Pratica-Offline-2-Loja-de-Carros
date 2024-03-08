ad8ce1398be7413e2551e6c1a984d24aa476d21e
João Sales <joaosales911@gmail.com>
2024-03-08 00:20:06 -0300

    feat: modelando controller de Servidor

--- Loja_de_Carros/src/controller/Servidor.java
+++ Loja_de_Carros/src/controller/Servidor.java
@@ -1,7 +1,27 @@
 package controller;
 
-import model.entity.Carro;
 
 public class Servidor {
-	private Carro carros[];
+	private int porta;
+	private String ip;
+	
+	public Servidor(int porta, String ip) {
+		setPorta(porta);
+		setIp(ip);
+	}
+	
+	public int getPorta() {
+		return porta;
+	}
+	public void setPorta(int porta) {
+		this.porta = porta;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	
+	
 }
